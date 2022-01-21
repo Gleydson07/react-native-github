38c310a40a0f34fc1640b4c6f794e9d3e6d688c8
gleydson.santos <gleydson.santos@vipy.com.br>
2022-01-21 20:09:36 -0300

    resolve challenge to add animations

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
+  Extrapolate,
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
   withTiming
@@ -19,15 +21,28 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
+      opacity: interpolate(cardOpacity.value, [0, 1], [0, 1]),
+      transform: [
+        {
+          translateX: interpolate(cardOffset.value,
+            [0.25 * displayWidth, 0],
+            [0.25 * displayWidth, 0]
+          )
+        }
+      ]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOffset.value = withTiming(
+      0,
+      {duration: 1000}
+    )
+
+    cardOpacity.value = withTiming(
+      1,
+      {duration: 1000}
+    )
   }, []);
 
   return (
